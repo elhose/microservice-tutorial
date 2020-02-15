5803b7735940727b7b70179bcc650b3e08ebdbb8
Jacek <jaceksawko95@gmail.com>
2020-02-15 21:33:03 +0100

    simple api versioning

---
+++ src/main/java/tt/psc/com/microservicetutorial/web/controller/v2/BeerControllerV2.java
@@ -0,0 +1,52 @@
+package tt.psc.com.microservicetutorial.web.controller.v2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tt.psc.com.microservicetutorial.web.model.v2.BeerDtoV2;
+import tt.psc.com.microservicetutorial.web.services.v2.BeerServiceV2;
+
+import java.util.UUID;
+
+@RequestMapping("api/v2/beer")
+@RestController
+public class BeerControllerV2 {
+    private final BeerServiceV2 beerServiceV2;
+
+    @Autowired
+    public BeerControllerV2(BeerServiceV2 beerServiceV2) {
+        this.beerServiceV2 = beerServiceV2;
+    }
+
+    @GetMapping({"/{beerId}"})
+    public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId) {
+        return new ResponseEntity<>(beerServiceV2.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity putBeer(@RequestBody BeerDtoV2 beerDtoV2) {
+
+        BeerDtoV2 savedBeer = beerServiceV2.saveBeer(beerDtoV2);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        httpHeaders.add("Location", "http://localhost:8080/api/v1/beer" + savedBeer.getId().toString());
+        return new ResponseEntity(httpHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity editBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDtoV2 beerDtoV2) {
+
+        beerServiceV2.updateBeer(beerDtoV2, beerId);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerServiceV2.deleteBeer(beerId);
+    }
+}
---
+++ src/main/java/tt/psc/com/microservicetutorial/web/services/v2/BeerServiceV2.java
@@ -0,0 +1,15 @@
+package tt.psc.com.microservicetutorial.web.services.v2;
+
+import tt.psc.com.microservicetutorial.web.model.v2.BeerDtoV2;
+
+import java.util.UUID;
+
+public interface BeerServiceV2 {
+    BeerDtoV2 getBeerById(UUID beerId);
+
+    BeerDtoV2 saveBeer(BeerDtoV2 beerDtoV2);
+
+    void updateBeer(BeerDtoV2 beerDtoV2, UUID beerId);
+
+    void deleteBeer(UUID beerId);
+}
