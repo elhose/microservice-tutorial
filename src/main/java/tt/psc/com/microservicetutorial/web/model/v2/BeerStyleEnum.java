705f407d6ed5e651d7fddb0c6b0e1796f4327a59
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-02-18 10:38:47 +0100

    merging changes

--- src/main/java/tt/psc/com/microservicetutorial/web/model/v2/BeerStyleEnum.java
+++ src/main/java/tt/psc/com/microservicetutorial/web/model/v2/BeerStyleEnum.java
@@ -1,9 +1,5 @@
 package tt.psc.com.microservicetutorial.web.model.v2;
 
-<<<<<<< HEAD
-public class BeerStyleEnum {
-=======
 public enum  BeerStyleEnum {
     LAGER, PILSNER, ALE, STOUT, GOSE, IPA
->>>>>>> 5803b7735940727b7b70179bcc650b3e08ebdbb8
 }
